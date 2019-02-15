30782c26fbbf7bbdb65559298083882353072c58
Terry Brady <terrywbrady@gmail.com>
2019-02-15 12:09:11 -0800

    HELP-17384

--- webapp/barcode.js
+++ webapp/barcode.js
@@ -54,15 +54,15 @@ var COLORMAP = [
   {status: "PASS",       color: "white",        nickname: "white",           desc: "Information is valid.  No action required."},
   {status: STAT_FAIL,    color: "pink",         nickname: "pink",            desc: "Retrieval failed.  Try to refresh again.  File a ticket with LIT if the issue persists."},
   {status: "NOT-FOUND",  color: "coral",        nickname: "red",             desc: "No Alma data for barcode.  Attach flag and pull item."},
-  {status: "META-CALL",  color: "darkorange",   nickname: "electric orage",  desc: "Bad Call Number.  Attach flag and send to Metadata Services for correction."},
+  {status: "META-CALL",  color: "darkorange",   nickname: "electric orange", desc: "Bad Call Number.  Attach flag and send to Metadata Services for correction."},
   {status: "META-TTL",   color: "lightskyblue", nickname: "blue",            desc: "Bad Title.  Attach flag and send to Metadata Services for correction."},
   {status: "META-VOL",   color: "lightgreen",   nickname: "mint green",      desc: "Bad Volume.  Attach flag and send to Metadata Services for correction."},
-  {status: "PULL-STAT",  color: "goldenrod",    nickname: "goldenrod",       desc: "Incorrect status code. Attach flag and pull item.  Send to Access Services for correction."},
+  {status: "PULL-STAT",  color: "goldenrod",    nickname: "goldenrod",       desc: "Incorrect process type. Attach flag and pull item.  Send to Access Services for correction."},
   {status: "PULL-LOC",   color: "yellow",       nickname: "yellow",          desc: "Incorrect location. Attach flag and pull item.  Send to Access Services for correction."},
   {status: "PULL-SUPP",  color: "tan",          nickname: "tan",             desc: "Bib is marked as suppressed. Attach flag and pull item.  Send to Access Services for correction."},
   {status: "PULL-HSUPP", color: "violet",       nickname: "purple",          desc: "Holding is suppressed. Attach flag and pull item.  Send to Access Services for correction."},
-  {status: "PULL-DUE",   color: "chartreuse",   nickname: "electric green",  desc: "Item is checked out in ALma. Attach flag and pull item.  Send to Access Services for correction."},
-  {status: "PULL-MULT",  color: "grey",         nickname: "grey",            desc: "Multiple issues. Attach flags and pull item.  Send to Access Services for correction."},
+  {status: "PULL-DUE",   color: "chartreuse",   nickname: "electric green",  desc: "Item is checked out in Alma. Attach flag and pull item.  Send to Access Services for correction."},
+  {status: "PULL-MULT",  color: "grey",         nickname: "grey",            desc: "Multiple problems. Attach flags and pull item.  Send to Access Services for correction."},
 ];
 var STATUSES = [];
 
